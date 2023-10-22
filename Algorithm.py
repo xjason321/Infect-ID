764f3a7fdd8464662cecb363a4bc105234353c74
tsuki <xjason321@gmail.com>
2023-10-21 19:55:32 -0700

    Add UI to Network

--- Algorithm.py
+++ Algorithm.py
@@ -8,12 +8,12 @@ class Algorithm():
       self.nodeLikelihoods = [1] * size
       # self.percentSamplesAlloted = 0.2
       # self.percentTracesAlloted = 0.05
-      
+
       self.negativeEffectBias = negativeEffectBias
-    
+
       self.positiveBias = positiveBias
       self.positiveEffectBias = positiveEffectBias
-      
+
       self.similarityWeight = similarityWeight
 
     def InitialScan(self, nodes):
@@ -28,13 +28,13 @@ class Algorithm():
                 self.nodeLikelihoods[node.id] += self.positiveBias
                 for neighbor in node.connections:
                     self.nodeLikelihoods[neighbor.id] += self.positiveEffectBias
-    
+
     def ChooseOneToSample(self, player):
         # Find most likely after initial scan
         self.InitialScan(player.nodes)
 
         save = []
-        
+
         for node in player.sampled:
             save.append((player.nodes[node].id, self.nodeLikelihoods[node]))
             self.nodeLikelihoods[node] = -999
@@ -43,26 +43,26 @@ class Algorithm():
 
         for Id, likelihood in save:
             self.nodeLikelihoods[Id] = likelihood
-        
+
         # Find most likely nodes and sample them
         likelyNodes = []
         for node in player.nodes:
             if self.nodeLikelihoods[node.id] == mostLikely:
                 likelyNodes.append(node.id)
-        
+
         return random.choice(likelyNodes) 
-       
+
     def FindDifference(self, list1, list2):
         # Calculate Jaccard similarity
         intersection = len(set(list1).intersection(set(list2)))
         union = len(set(list1).union(set(list2)))
         jaccard_similarity = intersection / union
-        
+
         return jaccard_similarity
 
     def TraceSpreadPattern(self, Player, target_node):
         new_player = copy.deepcopy(Player)
-        
+
         # Reset infections
         for node in new_player.nodes:
             node.state = 0
@@ -75,7 +75,7 @@ class Algorithm():
         difference = math.fabs(self.FindDifference(infectionsForNewPlayer, infectionsForCurrentPlayer))
 
         self.nodeLikelihoods[Player.nodes[target_node].id] -= (difference * self.similarityWeight)
-    
+
     def getSortedIds(self):
         indexed_arr = [(value, index) for index, value in enumerate(self.nodeLikelihoods)]
 
